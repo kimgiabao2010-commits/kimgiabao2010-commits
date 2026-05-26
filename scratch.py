9bfc0bb01d37d0dd4b5503e6432add83f346997d
kimgiabao2010 <kimgiabao2010@gmail.com>
2026-05-26 10:33:20 +0700

    chore: untrack dataset/log files already in gitignore and clean up temp test scripts

--- scratch.py
+++
@@ -1,6 +0,0 @@
-import urllib.request, json
-req = urllib.request.Request('http://127.0.0.1:8000/api/scan', data=b'{"text": "../../../etc/passwd"}', headers={'Content-Type': 'application/json', 'Origin': 'http://localhost'})
-try:
-  urllib.request.urlopen(req)
-except Exception as e:
-  print(e.headers)
